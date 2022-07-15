e30a856dbf1ec7c484ca180f0610b8232852aa43
ibukun21 <omodaraibukun211@gmail.com>
2022-07-15 02:14:24 +0100

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef mainh
 #define mainh
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,7 +11,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
 int _putchar(char c);
-
 #endif
