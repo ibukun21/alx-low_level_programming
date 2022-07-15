8e0615f827102dfb24359e4600e8c92ac1578cc4
ibukun21 <omodaraibukun211@gmail.com>
2022-07-15 01:58:26 +0100

    commit message for 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates value it points to to 98
+ * reset_to_98 -function that takes a pointer to am int as parameter and
+ * updates the value it points to to 98
  *
- * @n: pointer
+ * @n: this is the entry
+ *
  */
 
 void reset_to_98(int *n)
 {
-*n = 98;
+      *n = 98;
 }
