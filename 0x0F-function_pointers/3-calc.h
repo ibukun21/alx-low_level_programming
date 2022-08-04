4579a5568e663b095a9081a0174963b9e6633415
ibukun21 <83639540+ibukun21@users.noreply.github.com>
2022-08-05 00:08:54 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,13 @@
-#ifndef CALC_H
-#define CALC_H
+#ifndef CALCH
+#define CALCH
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
 
 /**
  * struct op - Struct op
@@ -9,15 +17,8 @@
  */
 typedef struct op
 {
-char *op;
-int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
-#endif /* CALC_H */
+#endif
