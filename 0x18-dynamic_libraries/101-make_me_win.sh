57e53f0c96c06f91b024d987a0b86df7fa83506f
ibukun21 <83639540+ibukun21@users.noreply.github.com>
2022-10-04 01:10:12 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/ibukun21/alx-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=../jackpot.so
+wget -P ../ https://github.com/ibukun21/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libfake.so
+export LD_PRELOAD=../libfake.so
