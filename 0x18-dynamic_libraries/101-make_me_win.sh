274d1925c31d8c21dfb69208aff7ef99b4b5cc9e
ibukun21 <83639540+ibukun21@users.noreply.github.com>
2022-10-04 01:19:31 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/ibukun21/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libfake.so
-export LD_PRELOAD=../libfake.so
+wget -O /tmp/win.so https://github.com/ibukun21/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
